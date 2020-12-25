cef40193939cba7f2890f52458d380c539b76a47
Dikshita Kambri <64687537+dikshitakambri@users.noreply.github.com>
2020-12-25 21:20:14 +0530

    fizzBuzz game

---
+++ fizzBuzz_game.js
@@ -0,0 +1,25 @@
+/* This is simple fizzBuzz game. In this game, when ther's multiple of 3 it will give output "Fizz",when ther's multiple of 5 it will give output "Buzz",and when there's
+multiple of both 3 and 5 it will give output "BuzzFizz" */
+
+var container = []
+count = 1;
+
+function game() {
+  
+  if(count % 5 ==0 && count % 3==0){
+    container.push("BuzzFizz");
+  }
+  else if(count % 5 ==0 ){
+    container.push("Buzz");
+  }
+  else if(count % 3 ==0 ){
+    container.push("Fizz");
+  }
+  else {
+    container.push(count);
+  }
+  count++;
+  
+  console.log(container);
+}
+game();
